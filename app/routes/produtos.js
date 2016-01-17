fde50a1dee759418f67351488f9485625b104bac
AndersonBrantes <andersonbrantes@gmail.com>
2016-01-17 11:46:00 -0200

    separando arquivos da conexao com o banco

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,9 +1,14 @@
-
-
+var connectionFactory = require('../infra/connectionFactory');
 
 module.exports = function(app) {
 
   app.get('/produtos', function(req, res){
-    res.render("produtos/lista");
+    var connection = connectionFactory();
+
+    connection.query('SELECT * FROM livros', function(err, results){
+      res.render('produtos/lista', { lista: results });
+    });
+
+    connection.end();
   });
 }

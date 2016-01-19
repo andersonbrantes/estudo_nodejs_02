e1f57bf1dabc9106910f2933e3c115ca573b8096
AndersonBrantes <andersonbrantes@gmail.com>
2016-01-18 23:09:44 -0200

    organizando codigo com express-load

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,10 +1,15 @@
 var mysql = require('mysql');
 
-module.exports = function() {
+function createDBConnection() {
   return mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'casadocodigo'
   });
+}
+
+// Wrapper
+module.exports = function() {
+  return createDBConnection;
 }

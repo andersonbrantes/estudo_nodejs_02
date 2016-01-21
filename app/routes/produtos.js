c4af730fa8dd8df3ca148220fceeb6ee9380572e
AndersonBrantes <andersonbrantes@gmail.com>
2016-01-20 23:41:16 -0200

    simulando classe para organizar codigo

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -3,8 +3,9 @@ module.exports = function(app) {
 
   app.get('/produtos', function(req, res){
     var connection = app.infra.connectionFactory();
+    var produtosBanco = new app.infra.ProdutosDAO(connection);
 
-    connection.query('SELECT * FROM livros', function(err, results){
+    produtosBanco.lista(function(err, results){
       res.render('produtos/lista', { lista: results });
     });
 
